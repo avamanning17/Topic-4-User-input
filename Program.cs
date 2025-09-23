6610b93a118e5f81ccab3ab77ed2cb307d2bc01b
Ava Manning <362579849@GoTVDSB.ca>
2025-09-23 09:41:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Topic_4_User_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String name;
+
+            //Console.Write("What is your name? ");
+            //name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Hello " + name.ToUpper());
+
+            Console.WriteLine();
+            Double price, discount;
+            Console.WriteLine("What is the price?");
+            Double.TryParse(Console.ReadLine(), out price);
+            discount = price * 0.2;
+            price = price - discount;
+            Console.WriteLine("The price is " + price.ToString("C"));
+            Console.WriteLine();
+
+            //Console.WriteLine(price = (3.00 / 20%));
+        }
+    }
+}
